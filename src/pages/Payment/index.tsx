7859b081290a206a359453eaa7e45f95f0cb6d97
Rafael Sangali <rafaelsangali@outlook.com>
2022-08-24 13:58:01 -0300

    fix: input disable on payment

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -70,6 +70,7 @@ export default function Payment() {
               type="number"
               name="exchange"
               id="exchange"
+              disabled
             />
           </label>
           <label className="font-extrabold grid"
